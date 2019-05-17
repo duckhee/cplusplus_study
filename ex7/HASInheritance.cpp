e25e536487e2154d7b889650ab7a2dc530c8cd79
duckhee <fain9301@yahoo.com>
2019-05-17 13:21:39 +0900

    HASinheritance add

---
+++ ex7/HASInheritance.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Gun
+{
+    private:
+        int bullet;
+    public:
+        Gun(int num):bullet(num)
+        {
+
+        }
+        void Shot()
+        {
+            cout<<"BANNG!"<<endl;
+            bullet--;
+        }
+};
+
+class Police: public Gun
+{
+    private:
+        int handcuffs;
+    public:
+        Police(int bnum, int bcuff):Gun(bnum), handcuffs(bcuff)
+        {}
+        void PutHandcuff()
+        {
+            cout<<"SNAP!"<<endl;
+            handcuffs--;
+        }
+};
+
+int main()
+{
+    Police pman(5, 3);
+    pman.Shot();
+    pman.PutHandcuff();
+    return 0;
+}
