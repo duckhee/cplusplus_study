ff11b1e4cfe97f88d228e1d4bc6bc709aa3f8a07
duckhee <fain9301@yahoo.com>
2019-05-17 17:03:59 +0900

    add source

--- ex8/EmployeeManager3.cpp
+++ ex8/EmployeeManager3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
+
 
 class Employee
 {
@@ -36,6 +38,59 @@ class PermanentWorker:public Employee
             cout<<"Salary ::: "<<salary<<endl;
         }
 };
+
+
+class TemporaryWorker : public Employee
+{
+private:
+	int workTime;
+	int payPerHour;
+public:
+	TemporaryWorker(char * name, int pay)
+		: Employee(name), workTime(0), payPerHour(pay)
+	{  }
+	void AddWorkTime(int time)
+	{
+		workTime+=time;
+	}
+	int GetPay() const
+	{
+		return workTime*payPerHour;
+	}
+	void ShowSalaryInfo() const
+	{
+		ShowYourName();
+		cout<<"salary: "<<GetPay()<<endl<<endl;
+	}
+};
+
+
+class SalesWorker : public PermanentWorker
+{
+private:
+	int salesResult;    // �� �ǸŽ���
+	double bonusRatio;    // �󿩱� ���� 
+public:
+	SalesWorker(char * name, int money, double ratio)
+		: PermanentWorker(name, money), salesResult(0), bonusRatio(ratio)
+	{  }
+	void AddSalesResult(int value)
+	{
+		salesResult+=value;
+	}
+	int GetPay() const
+	{
+		return PermanentWorker::GetPay()       // PermanentWorker�� GetPay �Լ� ȣ��
+					+ (int)(salesResult*bonusRatio);
+	}
+	void ShowSalaryInfo() const
+	{
+		ShowYourName();
+		cout<<"salary: "<<GetPay()<<endl<<endl;
+	}
+};
+
+
 
 class EmployeeHandler
 {
@@ -86,9 +141,18 @@ int main()
 
     handler.AddEmployee(new PermanentWorker("KIM", 1000));
     handler.AddEmployee(new PermanentWorker("LEE", 1500));
-    handler.AddEmployee(new PermanentWorker("JUN", 2000));
+
+    TemporaryWorker *alba = new TemporaryWorker("JUNG", 700);
+    alba->AddWorkTime(5);
+    handler.AddEmployee(alba);
+
+    SalesWorker *seller = new SalesWorker("HONG", 1000, 0.1);
+    seller->AddSalesResult(7000);
+    handler.AddEmployee(seller);
 
     handler.ShowAllSalaryInfo();
+
     handler.ShowTotalSalary();
+
     return 0;
 }
