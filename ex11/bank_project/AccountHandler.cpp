0c0cc777744669656c8398649b34b542d355f72c
duckhee <fain9301@yahoo.com>
2019-06-03 15:36:50 +0900

    example 11 bank end

---
+++ ex11/bank_project/AccountHandler.cpp
@@ -0,0 +1,161 @@
+#include "BankingCommonDecl.h"
+#include "AccountHandler.h"
+#include "Account.h"
+#include "NormalAccount.h"
+#include "HighCreditAccount.h"
+
+void AccountHandler::ShowMenu() const
+{
+    cout<<"-----menu-----"<<endl;
+    cout<<"1. 계좌 개설"<<endl;
+    cout<<"2. 입금"<<endl;
+    cout<<"3. 출금"<<endl;
+    cout<<"4. 계좌 정보 전체 출력"<<endl;
+    cout<<"5. 프로그램 종료"<<endl;    
+}
+
+void AccountHandler::MakeAccount()
+{
+    int sel;
+    cout<<"계좌 종류 선택"<<endl;
+    cout<<"1. 보통 예금 계좌"<<endl;
+    cout<<"2. 신용 신뢰 계좌"<<endl;
+    cout<<"select : ";
+    cin>>sel;
+
+    if(sel == NORMAL)
+    {
+        MakeNormalAccount();
+    }else
+    {
+        MakeCreditAccount();
+    }
+}
+
+void AccountHandler::MakeNormalAccount()
+{
+    int id;
+    char name[NAME_LEN];
+    int balance;
+    int interRate;
+
+    cout<<"보통 예금 계좌 개설"<<endl;
+    cout<<"account ID : ";
+    cin>>id;
+    cout<<"name : ";
+    cin>>name;
+    cout<<"입금액 : ";
+    cin>>balance;
+    cout<<"이자율 : ";
+    cin>>interRate;
+    cout<<endl;
+
+    accArr[accNum++] = new NormalAccount(id, balance, name, interRate);
+
+}
+
+void AccountHandler::MakeCreditAccount()
+{
+    int id;
+    char name[NAME_LEN];
+    int balance;
+    int interRate;
+    int creditLevel;
+
+    cout<<"신용 신뢰 계좌 개설"<<endl;
+    cout<<"account ID : ";
+    cin>>id;
+    cout<<"name : ";
+    cin>>name;
+    cout<<"입금액 : ";
+    cin>>balance;
+    cout<<"이자율 : ";
+    cin>>interRate;
+    cout<<"신용 등금 (1toA, 2toB, 3toC) : ";
+    cin>>creditLevel;
+    cout<<endl;
+
+    switch(creditLevel)
+    {
+        case 1:
+            accArr[accNum++] = new HighCreditAccount(id, balance, name, interRate, LEVEL_A);
+        break;
+        case 2:
+            accArr[accNum++] = new HighCreditAccount(id, balance, name, interRate, LEVEL_B);
+        break;
+        case 3:
+            accArr[accNum++] = new HighCreditAccount(id, balance, name, interRate, LEVEL_C);
+        break;
+    }    
+}
+
+void AccountHandler::DepositMoney()
+{
+    int money;
+    int id;
+    cout<<"입 금"<<endl;
+    cout<<"account ID : ";
+    cin>>id;
+    cout<<"입 금 액 : ";
+    cin>>money;
+
+    for(int i = 0; i < accNum; i++)
+    {
+        if(accArr[i]->GetAccID() == id)
+        {
+            accArr[i]->Deposit(money);
+            cout<<"입금 완료"<<endl<<endl;
+            return ;
+        }
+    }
+    cout<<"유효 하지 않는 ID 입니다."<<endl<<endl;
+}
+
+void AccountHandler::WithdrawMoney()
+{
+    int money;
+    int id;
+    cout<<"출금"<<endl;
+    cout<<"account ID : ";
+    cin>>id;
+    cout<<"출금액 : ";
+    cin>>money;
+
+    for(int i = 0; i < accNum; i++)
+    {
+        if(accArr[i]->GetAccID() == id)
+        {
+            if(accArr[i]->Withdraw(money) == 0)
+            {
+                cout<<"잔액 부족"<<endl<<endl;
+                return;
+            }
+            cout<<"출금 완료 "<<endl<<endl;
+            return;
+        }
+    }
+    cout<<"유효하지 않는 ID 입니다."<<endl<<endl;
+}
+
+
+AccountHandler::AccountHandler() : accNum(0)
+{
+
+}
+
+void AccountHandler::ShowAllAccInfo() const
+{
+    for(int i = 0; i < accNum; i++)
+    {
+        accArr[i]->ShowAccInfo();
+        cout<<endl;
+    }
+}
+
+AccountHandler::~AccountHandler()
+{
+    for(int i = 0; i < accNum; i++)
+    {
+        delete accArr[i];
+    }
+}
---
+++ ex11/bank_project/NormalAccount.h
@@ -0,0 +1,23 @@
+#ifndef __NORMARL_ACCOUNT_H__
+#define __NORMARL_ACCOUNT_H__
+
+#include "Account.h"
+
+class NormalAccount:public Account
+{
+    private:
+        int interRate;
+    public:
+        NormalAccount(int ID, int money, char *name, int rate)
+        :Account(ID, money, name), interRate(rate)
+        {
+
+        }
+        virtual void Deposit(int money)
+        {
+            Account::Deposit(money);
+            Account::Deposit(money*(interRate/100.0));
+        }
+};
+
+#endif
